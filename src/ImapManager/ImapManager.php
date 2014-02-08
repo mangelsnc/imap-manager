f8e5c64fd1ae34efd127f7e20ac41cccf9edd2d9
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2014-02-08 19:25:09 +0100

    Many refactors
    Refactor constructor and added more methods

--- src/ImapManager/ImapManager.php
+++ src/ImapManager/ImapManager.php
@@ -3,20 +3,82 @@
 namespace ImapManager;
 
 use ImapManager\Mailbox;
+use ImapManager\ConnectionString;
+use ImapManager\Exception;
 
 class ImapManager
 {
 
-    private $mailbox;
+    private $imapStream;
+    private $connectionString;
     private $user;
     private $password;
  
-    public function __construct(Mailbox $mailbox, $user, $password)
+    public function __construct($connectionString, $user, $password)
     {
-        $this->mailbox = $mailbox;
+        $this->connectionString = $connectionString;
         $this->user = $user;
         $this->password = $password;
+
+        $this->connect();
     }
 
+    public function getImapStream()
+    {
+        return $this->imapStream;
+    }
+
+    public function getConnectionString()
+    {
+        return $this->connectionString->getConnectionString();
+    }
+
+    public function connect()
+    {
+        $this->imapStream = @imap_open(
+                                    $this->connectionString->getConnectionString(), 
+                                    $this->user, 
+                                    $this->password
+        );
+
+        if(false === $this->imapStream) {
+            throw new Exception\ConnectionException(imap_last_error());          
+        }
+    }
+
+    public function changeMailbox($connectionString)
+    {
+        $this->connectionString = $connectionString;
+        $this->connect();
+    }
+
+    public function listAvailableMailboxes()
+    {
+        return imap_list($this->imapStream, $this->connectionString->getConnectionString(), '*');
+    }
+
+    /*
+    public function getMailBoxes()
+    {
+        $mailboxes = imap_getmailboxes($this->imapStream, $this->connectionString->getConnectionString(), '*');
+
+        if(is_array($mailboxes)) {
+            $mailboxesObj = array();
+            
+            foreach ($mailboxes as $mailbox) {
+                $mailboxObj = new MailBox();
+                $mailboxObj->setName($mailbox->name);
+                $mailboxObj->setDelimiter($mailbox->delimiter);
+                $mailboxObj->setAttributtes($mailbox->attributes);
+
+                $mailboxesObj[]=$mailboxObj;
+            }
+
+            return $mailboxesObj;
+        }
+
+        return false;
+    }
+    */
     
 }
